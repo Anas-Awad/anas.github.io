9aa248618e9a6ffc91a90ebee1b660c0c1cf8c04
hossam9866 <77072140+hossam9866@users.noreply.github.com>
2022-10-14 04:14:04 +0200

    hh

--- js/data.js
+++ js/data.js
@@ -7,8 +7,10 @@ window.onload = () => {
       let data = JSON.parse(this.responseText);
       setProjectsData(data);
       setHomeData(data);
-      setAboutData(data);
       setBackGrounds(data["background_images"]);
+      setFilters(data["filters"]);
+      filterData(data);
+      setCerts(data["certifications"]);
     }
   };
   function setHomeData(data) {
@@ -20,34 +22,34 @@ window.onload = () => {
       .setAttribute("src", "img/" + data["home"]["imageURL"]);
   }
 
-  function setAboutData(data) {
-    let aboutElement = document.querySelector(".second-content");
-    aboutElement.querySelector(".left-content h2").innerText =
-      data["about"]["header"];
-    aboutElement.querySelector(".left-content p").innerText =
-      data["about"]["paragraph"];
-
-    aboutElement
-      .querySelector(".right-image img")
-      .setAttribute("src", `${"img/" + data["about"]["image"]}`);
-  }
-
   function setProjectsData(data) {
     let projectsElement = document.querySelector("#projcets");
     for (let i = 0; i < data["projects"].length; i++) {
       let el = document.createElement("div");
-      el.innerHTML = `<div class="col-md-4 col-sm-6">
+      el.innerHTML = `
+              <div class="col-md-4 col-sm-6 single-project" data-filter=${data[
+                "projects"
+              ][i]["filter"].replaceAll(" ", "_")}>
                 <div class="item">
                   <div class="thumb">
-                    <a target="blank" href=${
-                      data["projects"][i]["url"]
-                    } data-lightbox="image-1"
+                    <a
+                      data-lightbox="image-1"
                       ><div class="hover-effect">
                         <div class="hover-content">
                           <h2>${data["projects"][i]["name"]}</h2>
-                          <p>
-                           ${data["projects"][i]["description"]}
+                          <p style="color: white; margin-bottom: 20px">
+                            ${data["projects"][i]["description"]}
                           </p>
+                         ${
+                           data["projects"][i]["url"] == ""
+                             ? ""
+                             : `<a target="_blank" class="buttons" href=${data["projects"][i]["url"]}>Code</a>`
+                         }
+                         ${
+                           data["projects"][i]["url2"] == ""
+                             ? ""
+                             : `<a target="_blank" class="buttons" href=${data["projects"][i]["url2"]}>${data["projects"][i]["button2"]}</a>`
+                         }
                         </div>
                       </div></a
                     >
@@ -64,15 +66,65 @@ window.onload = () => {
   function setBackGrounds(data) {
     document.getElementById(
       "1"
-    ).style.cssText = `background-image:url(img/${data["home"]});`;
-    document.getElementById(
-      "2"
     ).style.cssText = `background-image:url(img/${data["about"]});`;
     document.getElementById(
+      "2"
+    ).style.cssText = `background-image:url(img/${data["projects"]});`;
+    document.getElementById(
       "3"
-    ).style.cssText = `background-image:url(img/${data["work"]});`;
+    ).style.cssText = `background-image:url(img/${data["certifications"]});`;
     document.getElementById(
       "4"
     ).style.cssText = `background-image:url(img/${data["contact"]});`;
+  }
+
+  function setFilters(data) {
+    let el = document.querySelector(".filter");
+    for (let i = 0; i < data.length; i++) {
+      let div = document.createElement("div");
+      div.innerHTML = `<a class="single-filter">${data[i]}</a>`;
+      el.appendChild(div);
+    }
+  }
+
+  function filterData(data) {
+    let filterElements = document.querySelectorAll(".single-filter");
+    let projects = document.querySelectorAll(".single-project");
+    console.log(projects[0].dataset.filter.replaceAll("_", " "));
+    for (let i = 0; i < filterElements.length; i++) {
+      filterElements[i].addEventListener("click", (target) => {
+        console.log(target.target.innerText);
+
+        if (target.target.classList.contains("active")) {
+          filterElements[0].click();
+          if (target.target.innerText != "All") {
+            target.target.classList.remove("active");
+          }
+        } else {
+          filterElements.forEach((e) => {
+            e.classList.remove("active");
+          });
+          target.target.classList.add("active");
+
+          projects.forEach((t) => {
+            if (target.target.innerText.toLowerCase() == "all") {
+              t.style.display = "block";
+            } else if (
+              t.dataset.filter.replaceAll("_", " ") ==
+              target.target.innerText.toLowerCase()
+            ) {
+              t.style.display = "block";
+            } else {
+              t.style.display = "none";
+            }
+          });
+        }
+      });
+    }
+  }
+
+  function setCerts(data) {
+    let el = document.querySelector(".certs");
+    console.log(el);
   }
 };
